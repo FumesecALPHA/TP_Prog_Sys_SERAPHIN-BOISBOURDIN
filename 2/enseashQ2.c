677a8595a1cf65900e0b499eaa09d7915045195f
FumesecALPHA <185381543+FumesecALPHA@users.noreply.github.com>
2025-12-22 03:22:33 +0100

    Add files via upload

---
+++ 2/enseashQ2.c
@@ -0,0 +1,53 @@
+#include "enseash.h"
+
+int main() {
+
+    // Message d'entrée du shell
+    const char* welcome_msg = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.";
+    const size_t len_welcome_msg = strnlen(welcome_msg, BUFFER_SIZE);
+
+    write(STDOUT_FILENO, welcome_msg, len_welcome_msg);
+
+    const char* prefix = "\nenseash % ";
+    const size_t len_prefix = strnlen(prefix,BUFFER_SIZE);
+
+    char buffer[BUFFER_SIZE];
+
+    while (1){
+        //prefix
+        write(STDOUT_FILENO, prefix, len_prefix);
+
+        //read
+        ssize_t n = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        buffer[n-1] = '\0';
+
+        // fortune
+        if (strncmp(buffer, "fortune",strnlen("fortune",BUFFER_SIZE)) == 0){
+            int statuts;
+            int pid = fork();
+
+            if (pid!=0){
+                wait(&statuts);
+            }
+            else {
+                execlp("fortune","fortune",(char*)NULL);
+            }
+        }
+
+        // date
+        else if (strncmp(buffer, "date",strnlen("date",BUFFER_SIZE)) == 0){
+            int statuts;
+            int pid = fork();
+
+            if (pid!=0){
+                wait(&statuts);
+            }
+            else {
+                execlp("date","date",(char*)NULL);
+            }
+        }
+
+        // écriture dans le shell
+        write(STDOUT_FILENO, buffer, n);
+    }
+}
